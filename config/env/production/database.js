b741271f6bf4595e227077f9cd4cbaa8245ba601
Pierre BAZIN <pierre.bazin.pro@gmail.com>
2022-08-10 16:47:50 +0000

    Update config/env/production/database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -4,10 +4,10 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: process.env.DATABASE_HOST,
-      port: process.env.DATABASE_PORT,
-      database: process.env.DATABASE_NAME,
-      user: process.env.DATABASE_USERNAME,
+      host: 'srv-captain--database',
+      port: 5432,
+      database: 'cocktailDb',
+      user: 'cocktailUser',
       password: process.env.DATABASE_PASSWORD,
       ssl: false
     },
